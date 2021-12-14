d2e549bb55d5cd7a4e0da59b0482e50448bcda2b
Code-xed <ngudulu@gmail.com>
2021-12-15 02:00:55 +0530

    Update dictionary.py

--- Dictionary-App/dictionary.py
+++ Dictionary-App/dictionary.py
@@ -6,10 +6,10 @@ dictionary = PyDictionary()
 def check_meaning(word):         # if user want to check meaning of word
     return dictionary.meaning(word)
 
-def check_antonym(word):         # if user want to check antonym of word
+def get_antonym(word):         # if user want to check antonym of word
     return dictionary.antonym(word)
 
-def check_synonym(word):         # if user want to check synonym of word
+def get_synonym(word):         # if user want to check synonym of word
     return dictionary.synonym(word)
 
 def translate(word,language):     # if user want to translate word into other language
@@ -18,13 +18,14 @@ def translate(word,language):     # if user want to translate word into other la
 
 
 def menu():
-    print("\n----------------------------------------------")
-    print("Enter 1 to check meaning of word")
-    print("Enter 2 to check antonym of word")
-    print("Enter 3 to check synonym of word")
-    print("Enter 4 to translate word to other language")
-    print("Enter 0 to the close dictionary")
-    print()
+    print('''
+----------------------------------------------
+Enter 1 to check meaning of word
+Enter 2 to get antonyms of word
+Enter 3 to get synonyms of word
+Enter 4 to translate word to other language
+Enter 0 to the close dictionary
+''')
 
     choice = int(input("Enter your choice: "))
 
@@ -34,35 +35,31 @@ def menu():
 while(True):
     word = input("\nEnter a word: ")
     user_choice = menu()
+    match user_choice:
+        case 0:
+            print("Dictionary is closed")
+            exit(0)
+        case 1:
+            meaning = check_meaning(word)
+            print(meaning)
+        case 2:
+            antonym = get_antonym(word)
+            print(antonym)
+        case 3:
+            synonym = get_synonym(word)
+            print(synonym)
+        case 4:
+            print('''Enter a language code in which you want to translate word
+Like for URDU language code is ur
+For ARABIC language code is ar
+For HINDI language code is hi
+''')
 
-    if user_choice == 0:
-        print("Dictionary is closed")
-        exit(0)
-
-    elif user_choice == 1:
-        meaning = check_meaning(word)
-        print(meaning)
-
-    elif user_choice == 2:
-        antonym = check_antonym(word)
-        print(antonym)
-
-    elif user_choice == 3:
-        synonym = check_synonym(word)
-        print(synonym)
-
-    elif user_choice == 4:
-        print("Enter a language code in which you want to translate word")
-        print("Like for URDU language code is ur ")
-        print("For ARABIC language code is ar ")
-        print("For HINDI language code is hi ")
-
-        lang_choice = input("Enter your choice: ")
-        trans = translate(word,lang_choice)
-        print(trans)
-
-    else:
-        print("Invalid choice!")
+            lang_choice = input("Enter your choice: ")
+            trans = translate(word, lang_choice)
+            print(trans)
+        case _:
+            print("Invalid choice!")
 
 #-------------------------------------------------------------
 
