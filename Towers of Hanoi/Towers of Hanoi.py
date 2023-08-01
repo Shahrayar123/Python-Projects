112026f0715dd6574f8eda015b750c385dcf4b67
Lizzon <lvdv4j@gmail.com>
2023-08-01 20:57:33 +0200

    Reformatted the code for better read-ability

--- Towers of Hanoi/Towers of Hanoi.py
+++ Towers of Hanoi/Towers of Hanoi.py
@@ -1,67 +1,69 @@
 class Node:
-  def __init__(self, value, link_node = None):
-    self.value = value
-    self.link_node = link_node
-    
-  def set_next_node(self, link_node):
-    self.link_node = link_node
-    
-  def get_next_node(self):
-    return self.link_node
-  
-  def get_value(self):
-    return self.value
+    def __init__(self, value, link_node=None):
+        self.value = value
+        self.link_node = link_node
+
+    def set_next_node(self, link_node):
+        self.link_node = link_node
+
+    def get_next_node(self):
+        return self.link_node
+
+    def get_value(self):
+        return self.value
+
 
 class Stack:
-  def __init__(self, name):
-    self.size = 0
-    self.top_item = None
-    self.limit = 1000
-    self.name = name
-  
-  def push(self, value):
-    if self.has_space():
-      item = Node(value)
-      item.set_next_node(self.top_item)
-      self.top_item = item
-      self.size += 1
-    else:
-      print("No more room!")
+    def __init__(self, name):
+        self.size = 0
+        self.top_item = None
+        self.limit = 1000
+        self.name = name
 
-  def pop(self):
-    if self.size > 0:
-      item_to_remove = self.top_item
-      self.top_item = item_to_remove.get_next_node()
-      self.size -= 1
-      return item_to_remove.get_value()
-    print("This stack is totally empty.")
+    def push(self, value):
+        if self.has_space():
+            item = Node(value)
+            item.set_next_node(self.top_item)
+            self.top_item = item
+            self.size += 1
+        else:
+            print("No more room!")
 
-  def peek(self):
-    if self.size > 0:
-      return self.top_item.get_value()
-    print("Nothing to see here!")
+    def pop(self):
+        if self.size > 0:
+            item_to_remove = self.top_item
+            self.top_item = item_to_remove.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_value()
+        print("This stack is totally empty.")
 
-  def has_space(self):
-    return self.limit > self.size
+    def peek(self):
+        if self.size > 0:
+            return self.top_item.get_value()
+        print("Nothing to see here!")
 
-  def is_empty(self):
-    return self.size == 0
-  
-  def get_size(self):
-    return self.size
-  
-  def get_name(self):
-    return self.name
-  
-  def print_items(self):
-    pointer = self.top_item
-    print_list = []
-    while(pointer):
-      print_list.append(pointer.get_value())
-      pointer = pointer.get_next_node()
-    print_list.reverse()
-    print("{0} Stack: {1}".format(self.get_name(), print_list))
-    
+    def has_space(self):
+        return self.limit > self.size
+
+    def is_empty(self):
+        return self.size == 0
+
+    def get_size(self):
+        return self.size
+
+    def get_name(self):
+        return self.name
+
+    def print_items(self):
+        pointer = self.top_item
+        print_list = []
+        while pointer:
+            print_list.append(pointer.get_value())
+            pointer = pointer.get_next_node()
+        print_list.reverse()
+        print("{0} Stack: {1}".format(self.get_name(), print_list))
+
+
 class TowersOfHanoi:
     def __init__(self, num_disks):
         self.num_disks = num_disks
@@ -127,6 +129,7 @@ class TowersOfHanoi:
                         return self.stacks[i]
             else:
                 print("Invalid choice. Please enter a valid option.")
+
 
 # Run the game
 if __name__ == "__main__":
