915bcb05e312f5963fa8aacc01666fa00b42c66b
gabiervo <125902454+gabiervo@users.noreply.github.com>
2023-08-14 17:51:25 -0300

    New Number Guessing Game with simple GUI
    An extension to the original number guessing game using simple tkinter GUI, has opportunities to improve upon styling of the individual widgets.

--- Number Guessing Game/main.py
+++ Number Guessing Game/main.py
@@ -1,4 +1,8 @@
 import random
+import tkinter as tk
+from tkinter import ttk
+import time
+
 
 """
  Funtion start() create the magic number, number of attemps, and ask user for input
@@ -6,38 +10,64 @@ import random
 def start():
     global magic_number
     magic_number = random.randint(1, 100)
+    print(magic_number)
+        
+mainWindow = tk.Tk()
+global attempts
+attempts = 0
 
-    global attempts
-    attempts = 0
+titleLabel = ttk.Label(master=mainWindow, text = 'Number Guessing Game', font=('Helvetica', 50, 'bold'))
 
-    print('---------------------------')
-    print('Guess a number between 1 and 100')
+global guessLabel
+guessLabel = ttk.Label(master=mainWindow, text = 'Guess a Number Between one and 100', font=('Helvetica', 230))
 
-# Function that checks if player won, if it won, returns True
-def check_win(player_guess):
-    if player_guess > magic_number:
-        print('Too big...')
-    elif player_guess < magic_number:
-        print('Too small')
-    elif player_guess == magic_number:
-        return True
+
+mainWindow.columnconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
+mainWindow.rowconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
+
+titleLabel.grid(row=0, sticky='N', pady=(20, 0), padx=40)
+
+corrLabel = ttk.Label(master=mainWindow, text='Guess a number between 1 and 100', font=('Helvetica', 20, 'italic'))
+
+
+entryVar = tk.StringVar()
 
 start()
 
-# Game loop
-while True:
-    # Take the player input
-    guess = int(input())
+def check_win():
+    global attempts
     attempts += 1
+    player_guess = entryVar.get()
+    try:
+        player_guess = int(player_guess)
+    except:
+        corrLabel.config(text='Invalid Input', foreground='red')
+        resetGuess()
+    
+    if player_guess > magic_number:
+        corrLabel.config(text='Too Big', foreground='red')
+        resetGuess()
+    elif player_guess < magic_number:
+        corrLabel.config(text='Too Small', foreground='red')
+        resetGuess()
+    elif player_guess == magic_number:
+        corrLabel.config(text='Correct, it took you ' + str(attempts) + " attempts.", foreground='green')
+        mainWindow.update()
+        time.sleep(1)
+        corrLabel.config(text='Restarting Game', foreground='blue')
+        mainWindow.update()
+        resetGuess()
+        start()
+    
+def resetGuess():
+    mainWindow.update()
+    time.sleep(1)
+    corrLabel.config(text='Guess a number between 1 and 100', foreground='white')
+  
+entry = ttk.Entry(master=mainWindow, textvariable=entryVar)
+entryButton = ttk.Button(master=mainWindow, text='Submit', command=check_win)    
+entry.grid(row=1, sticky='N', padx=(0, 110), pady=(30, 0))
+entryButton.grid(row=1, padx=(0, 180), sticky='EN', pady=(30, 0))
 
-    if check_win(guess):
-        print('You won! - Number of attempts: ' + str(attempts))
-
-        keep_playing = input('Keep playing?(y\\n)')
-        # If player want to keep the game, reset the number of attempts
-        if keep_playing == 'y':
-            attempts = 0
-            start()
-        # If player don't want to keep playing, quit the game
-        elif keep_playing == 'n':
-            quit()
+corrLabel.grid(sticky='N', pady=40)
+mainWindow.mainloop()
