444e7a3376f0de76642f3c9abecc934d8660cf4e
Buchi Umeano <108285540+B-uchi@users.noreply.github.com>
2023-08-15 00:04:12 +0100

    Ceasar Cipher Script

---
+++ ceasar_cipher/main.py
@@ -0,0 +1,50 @@
+import time
+
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' #symbols included in the cipher
+while True:
+    print('Do you want to (e)ncrypt or (d)ecrypt')
+    choice = input('> ')
+    if choice.lower().startswith('e'):
+        mode = 'encrypt'
+        break
+    if choice.lower().startswith('d'):
+        mode = 'decrypt'
+        break
+    print('Please enter e or d\n')
+
+while True:
+    maxKey = len(SYMBOLS) - 1
+    print('Enter a key')
+    response = input('> ')
+    if not response.isdecimal():
+        print('Enter numeric key')
+        response = input('> ')
+    if 0 <= int(response) <= maxKey:
+        key = int(response)
+        break
+
+
+print(f'Enter the text to {mode}')
+text = input('> ').upper()
+
+translated_text = ''
+
+for char in text: 
+    if char in SYMBOLS:
+        num = SYMBOLS.find(char)
+        if mode == 'encrypt':
+            num += key
+        if mode == 'decrypt':
+            num -= key
+        if num >= maxKey:
+            num -= maxKey
+        elif num < 0:
+            num += maxKey
+
+        translated_text += SYMBOLS[num]
+    else:
+        translated_text += char
+
+print('[+]Processing......')
+
+print('\n', translated_text)
