d74ce0d34b329bc28fa382f10736593cc2d61811
flrgx <4496289+drbree82@users.noreply.github.com>
2023-11-16 00:43:04 +0000

    Rewrote to be within a function
    the repetition can be added outside the function. This could be added to Pypi to install in pip3

--- Rock Paper Scissors Game/main.py
+++ Rock Paper Scissors Game/main.py
@@ -1,87 +1,28 @@
-#Rock Paper Scissors game by marsian
-print("\n\n\nRock Paper Scissors by marsian83\n\npresss Ctrl+. when you feel like exitting the game.")
-input("\n\n<<PRESS ENTER TO BEGIN>>\n")
-print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+#Grab randomizer to create computer's choice
+import random
 
-#Framework declaration
-from time import sleep
-from random import choice
-def game():
-    print("\n\n\n\n\n\n\n\nDecide your move, enter \"R\" for Rock, \"S\" for Scissors and \"P\"for Paper")
-    tempVal=input()
-    while tempVal != 'R' and tempVal != 'S' and tempVal != 'P' and tempVal != 'r' and tempVal != 's' and tempVal != 'p':
-        print("Decide your move, enter \"R\" for Rock, \"S\" for Scissors and \"P\"for Paper")
-        tempVal=input();
-    sleep(0.13)
-    if tempVal == "R" or tempVal == "r":
-        Pmove=1
-    elif tempVal == "P" or tempVal == "p":
-        Pmove=2
-    elif tempVal == "S" or tempVal == "s":
-        Pmove=3
-    else:
-        print("Custom_ERROR-01 : can't recognize user's input")
-        raise SystemExit(0);
-    sleep(0.13)
-    Cmove=choice(MoveSet)
-    if Cmove == 1:
-        cpumove='Rock'
-    elif Cmove == 2:
-        cpumove='Paper'
-    elif Cmove == 3:
-        cpumove='Scissor'
-    else:
-        print("Custom_ERROR-00 : CPU's move unrecognizable")
-        raise SystemExit(0)
-    del tempVal;
-    sleep(0.038)
-    return Cmove, Pmove, cpumove
-def decisive(Cmove,Pmove):
-    if [Cmove,Pmove] == [1,3] or [Cmove,Pmove] == [2,1] or [Cmove,Pmove] == [3,2]:
-        winner=1
-    elif [Cmove,Pmove] == [1,2] or [Cmove,Pmove] == [2,3] or [Cmove,Pmove] == [3,1]:
-        winner=2
-    elif [Cmove,Pmove] == [1,1] or [Cmove,Pmove] == [2,2] or [Cmove,Pmove] == [3,3]:
-        winner=0
-    else:
-        print("Custom_ERROR-03 : Set [cpumove,playermove] was assigned an unexpected value")
-        raise SystemExit(0)
-    return winner
-def Scoreboard(winner,cm,cs,ps):
-    if winner == 1:
-        print("\nCPU won the round by choosing",cm)
-        cs=cs+1
-    elif winner == 2:
-        print("\nPlayer won the round!")
-        ps=ps+1
-    else:
-        print("Custom_ERROR-02 : Scoreboard function recieved explicit values")
-        raise SystemExit(0)
-    print("\nSession Score:")
-    print("Player : ",ps)
-    print("CPU : ",cs)
-    return cs, ps
+#Function to take user's selection as int and return a win, loss or draw condition.
+def rps(user_choice):
+    choices = ["paper", "scissors", "rock"]
+#Exception handler
+    if user_choice < 0 or user_choice > 2:
+        raise ValueError("Invalid selection. Please enter a number between 0 and 2.")
+#variable assignment for passing cpu random selection and user selection into a list 
+    cpu_choice = random.randint(0, 2)
+    user_selection = choices[user_choice]
+    computer_selection = choices[cpu_choice]
 
-#Variable declaration
-MoveSet = [1,2,3]
-Cmove=0
-Pmove=0
-Cscore=0
-Pscore=0
+#Comparator
+    if user_selection == computer_selection:
+        return "It's a tie!"
+    elif (user_selection == "paper" and computer_selection == "rock") or (user_selection == "scissors" and computer_selection == "paper") or (user_selection == "rock" and computer_selection == "scissors"):
+        winning_statement = f"You win! User chose {user_selection}, computer chose {computer_selection}."
+        return winning_statement
+    else:
+        losing_statement = f"You lose! User chose {user_selection}, computer chose {computer_selection}."
+        return losing_statement
 
-#Driver Code
-while True:
-    Cmove,Pmove,cpumove=game()
-    winner=decisive(Cmove,Pmove)
-    while winner == 0:
-        print("WoW! A Draw!\nBoth Player and CPU have chosen",cpumove)
-        print("let's go again")
-        Cmove,Pmove,cpumove=game()
-        winner=decisive(Cmove,Pmove)
-    sleep(0.13)
-    cmCache=cpumove;
-    print("CPU - ",cmCache)
-    RoundWinner=winner
-    cs,ps=Scoreboard(RoundWinner,cmCache,Cscore,Pscore)
-    Cscore=cs; Pscore=ps
-    input("<<< PRESS ENTER TO CONTINUE >>>");
+# Example usage
+user_input = int(input("Please select paper(0), scissors(1) or rock (2): "))
+game_result = rps(user_input)
+print(game_result)
