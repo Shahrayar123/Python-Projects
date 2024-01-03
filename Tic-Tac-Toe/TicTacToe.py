6a71bac19be2b42bac307a6b9833c82745befa69
Saad <155129598+SaadZiaatharKhan@users.noreply.github.com>
2024-01-03 14:32:58 +0530

    AdvancedTicTacToe.py
    An Advanced Tic Tac Toe Game Which has Colors And Can Speak. 
    Needed Two Modules To Install Modules -
    Colorama - For Colors - Type command "pip install colorama" to install
    pyttsx3 - For Speech - Type Command "pip install pyttsx3" to install
    If speech is not working or error is occurring, download SAPI5 on Windows (NSSpeechSynthesizer for MAC Or eSpeak on Debian based Linux)

--- Tic-Tac-Toe/TicTacToe.py
+++ Tic-Tac-Toe/TicTacToe.py
@@ -1,138 +1,289 @@
-board = [' ' for i in range(10)]
+# By Saad Khan
 
-def insertLetter(letter,pos):
-    board[pos] = letter
+import colorama                         # For Adding Colors
+import time                             # For Time Related Functions
+import pyttsx3                          # For Speech
+import sys,subprocess                   # For Working In Shell
+from colorama import Fore,Back,Style    # For Adding Colors And Font Styles
+from time import strftime
 
+#colorama.init(autoreset=True)
 
-def spaceIsfree(pos):
-   return board[pos] == ' '
+voice=pyttsx3.init()
+voice.setProperty('rate',150)
 
-def printBoard(board):
-    print("   |   |   ")
-    print(" " +board[1] + " | "+ board[2]+ " | " + board[3])
-    print("   |   |   ")
-    print("-----------")
-    print("   |   |   ")
-    print(" " +board[4] + " | "+ board[5]+ " | " + board[6])
-    print("   |   |   ")
-    print("-----------")
-    print("   |   |   ")
-    print(" " + board[7] + " | " + board[8] + " | " + board[9])
-    print("   |   |   ")
+first_box=Fore.YELLOW+"1"
+second_box=Fore.YELLOW+"2"
+third_box=Fore.YELLOW+"3"
+forth_box=Fore.YELLOW+"4"
+fifth_box=Fore.YELLOW+"5"
+sixth_box=Fore.YELLOW+"6"
+seventh_box=Fore.YELLOW+"7"
+eigth_box=Fore.YELLOW+"8"
+ninth_box=Fore.YELLOW+"9"
 
-def isBoardFull(board):
-    if board.count(" ") > 1:
-        return False
-    else:
-        return True
+def main_game_program():
 
-def isWinner(b,l):    # b = board, l = letter
-    # check all possibilities
-    return ((b[1] == l and b[2] == l and b[3] == l) or (b[4] == l and b[5] == l and b[6] == l) or (b[7] == l and b[8] == l and b[9] == l) or (b[1] == l and b[4] == l and b[7] == l) or (b[2] == l and b[5] == l and b[8] == l) or (b[3] == l and b[6] == l and b[9] == l) or (b[1] == l and b[5] == l and b[9] == l) or (b[3] == l and b[5] == l and b[7] == l))
+    global first_box
+    global second_box
+    global third_box
+    global forth_box
+    global fifth_box
+    global sixth_box
+    global seventh_box
+    global eigth_box
+    global ninth_box
 
-def userMove():
-    run = True
+    first_box=Fore.YELLOW+"1"
+    second_box=Fore.YELLOW+"2"
+    third_box=Fore.YELLOW+"3"
+    forth_box=Fore.YELLOW+"4"
+    fifth_box=Fore.YELLOW+"5"
+    sixth_box=Fore.YELLOW+"6"
+    seventh_box=Fore.YELLOW+"7"
+    eigth_box=Fore.YELLOW+"8"
+    ninth_box=Fore.YELLOW+"9"
 
-    while run:
-        pos = input("Enter a position between 1 to 9: ")
+    day_and_date=strftime("%A, %D %B %Y")
+    time_in_12h_format=strftime("%I:%M:%S %p")
+    time_variable_for_greeting=int(strftime("%H"))
 
-        try:
-            pos = int(pos)
-            if (pos > 0) and (pos < 10):
-                if spaceIsfree(pos):
-                    run = False
-                    insertLetter("X" , pos)
-                else:
-                    print("Sorry this space is occupied")
+    welcome_intro="\n\n***** WELCOME TO TIC TAC TOE GAME *****"
+    print(Fore.YELLOW + Back.CYAN + Style.BRIGHT + welcome_intro.center(75))
+    print("\n\n")
+    print(Fore.GREEN + day_and_date)
+    print(Fore.GREEN + time_in_12h_format + "\n\n")
 
-            else:
-                print("Please enter a number range between 1 to 9")
+    playerx=input(Fore.MAGENTA + Back.YELLOW + "Who Wants To Be X : ")
+    playero=input(Fore.MAGENTA + Back.YELLOW +  "Who Wants To Be O : ")
+    print("\n")
+    do_you_want_sound=input(Fore.RED + Back.WHITE + "Do You Want Sound [Y \ N] : ")
+    print("\n\n")
 
-        except:
-            print("Please enter a number ")
+    variable_for_greeting_by_name=f"{playerx}, and, {playero}, welcome to tic tac toe game."
 
-def compMove():
-    possibleMoves = [x for x,letter in enumerate(board) if letter == " " and x != 0]
-    move = 0
+    def input_for_speak_greeting(final_greetings):
+        voice.say(final_greetings)
+        voice.runAndWait()
+        voice.stop()
 
-    for let in ['O','X']:
-        for i in possibleMoves:
-            boardCopy = board[:]
-            boardCopy[i] = let
+    def speak_greetings():
+        if (4<=time_variable_for_greeting<=12):
+            variable_for_voice_greeting_according_to_time=f"Good Morning, {variable_for_greeting_by_name}"
+            input_for_speak_greeting(variable_for_voice_greeting_according_to_time)
+        elif (12<time_variable_for_greeting<18):
+            variable_for_voice_greeting_according_to_time=f"Good Afternoon, {variable_for_greeting_by_name}"
+            input_for_speak_greeting(variable_for_voice_greeting_according_to_time)
+        elif (18<=time_variable_for_greeting<=22):
+            variable_for_voice_greeting_according_to_time=f"Good Evening, {variable_for_greeting_by_name}"
+            input_for_speak_greeting(variable_for_voice_greeting_according_to_time)
+        elif (22<time_variable_for_greeting<=23 or 0<=time_variable_for_greeting<4):
+            variable_for_voice_greeting_according_to_time=f"Hi, {variable_for_greeting_by_name}"
+            input_for_speak_greeting(variable_for_voice_greeting_according_to_time)
 
-            if isWinner(boardCopy,let):
-                move = i
-                return move
+    if (do_you_want_sound=="y" or do_you_want_sound=="Y" or do_you_want_sound=="yes" or do_you_want_sound=="Yes" or do_you_want_sound=="YES"):
+            speak_greetings()
+            
+    def game_layout():
+         print(Back.WHITE+Fore.GREEN+"....."+first_box+Fore.GREEN+"....."+"|"+"....."+second_box+Fore.GREEN+"....."+"|"+"....."+third_box+Fore.GREEN+".....")
+         print(Back.WHITE+Fore.GREEN+"-----------------------------------")
+         print(Back.WHITE+Fore.GREEN+"....."+forth_box+Fore.GREEN+"....."+"|"+"....."+fifth_box+Fore.GREEN+"....."+"|"+"....."+sixth_box+Fore.GREEN+".....")
+         print(Back.WHITE+Fore.GREEN+"-----------------------------------")
+         print(Back.WHITE+Fore.GREEN+"....."+seventh_box+Fore.GREEN+"....."+"|"+"....."+eigth_box+Fore.GREEN+"....."+"|"+"....."+ninth_box+Fore.GREEN+".....")
 
-    cornorOpen = []
-    for i in possibleMoves:
-        if i in [1,3,7,9]:
-            cornorOpen.append(i)
+    class speak_turns:
+        def playerx_speak_for_turn(speak_for_x_turn):
+            voice.say(speak_for_x_turn)
+            voice.runAndWait()
+            voice.stop()
+        def playero_speak_for_turn(speak_for_o_turn):
+            voice.say(speak_for_o_turn)
+            voice.runAndWait()
+            voice.stop()
+        def speak_for_invalid_option():
+            voice.say("Enter A Valid Option.")
+            voice.runAndWait()
+            voice.stop()
 
-    if len(cornorOpen) > 0:
-        move = selectRandom(cornorOpen)
-        return move
-
-    if 5 in possibleMoves:
-        move = 5
-        return move
-
-    edgeOpen = []
-    for i in possibleMoves:
-        if i in [2,4,6,8]:
-            edgeOpen.append(i)
-
-
-    if len(edgeOpen) > 0:
-        move = selectRandom(edgeOpen)
-        return move
-
-def selectRandom(list_):
-    import random
-    ln = len(list_)
-    r = random.randrange(0,ln)
-
-    return list_[r]
-
-
-def main():
-    print("Welcome to the tic tac toe game\n")
-    printBoard(board)
-
-    while not(isBoardFull(board)):
-        if not(isWinner(board, "O")):
-            userMove()
-            printBoard(board)
-
+    def playerx_turn():
+        subprocess.run('cls' , shell=True)
+        game_layout()
+        if (do_you_want_sound=="y" or do_you_want_sound=="Y" or do_you_want_sound=="yes" or do_you_want_sound=="Yes" or do_you_want_sound=="YES"):
+            speak_turns.playerx_speak_for_turn(f"{playerx}\'s turn")
+        turnx=input(f"{playerx}\'s turn : ")
+        global first_box
+        global second_box
+        global third_box
+        global forth_box
+        global fifth_box
+        global sixth_box
+        global seventh_box
+        global eigth_box
+        global ninth_box
+        if (turnx=="1"):
+            first_box=Fore.RED+"X"
+        elif (turnx=="2"):
+            second_box=Fore.RED+"X"
+        elif (turnx=="3"):
+            third_box=Fore.RED+"X"
+        elif (turnx=="4"):
+            forth_box=Fore.RED+"X"
+        elif (turnx=="5"):
+            fifth_box=Fore.RED+"X"
+        elif (turnx=="6"):
+            sixth_box=Fore.RED+"X"
+        elif (turnx=="7"):
+            seventh_box=Fore.RED+"X"
+        elif (turnx=="8"):
+            eigth_box=Fore.RED+"X"
+        elif (turnx=="9"):
+            ninth_box=Fore.RED+"X"
         else:
-            print("Sorry you loose! ")
-            break
+            if (do_you_want_sound=="y" or do_you_want_sound=="Y" or do_you_want_sound=="yes" or do_you_want_sound=="Yes" or do_you_want_sound=="YES"):
+                speak_turns.speak_for_invalid_option()
+            print(Fore.RED+"Enter A Valid Option.")
+            playerx_turn()
+        subprocess.run('cls' , shell=True)
 
-
-        if not(isWinner(board, "X")):
-            move = compMove()
-
-            if move == 0:
-                print("Tie game")
-
-            else:
-                insertLetter("O", move)
-                print(f"Computer place O on position {move}")
-                printBoard(board)
-
+    def playero_turn():
+        subprocess.run('cls' , shell=True)
+        game_layout()
+        if (do_you_want_sound=="y" or do_you_want_sound=="Y" or do_you_want_sound=="yes" or do_you_want_sound=="Yes" or do_you_want_sound=="YES"):
+            speak_turns.playero_speak_for_turn(f"{playero}\'s turn")
+        turno=input(f"{playero}\'s turn : ")
+        global first_box
+        global second_box
+        global third_box
+        global forth_box
+        global fifth_box
+        global sixth_box
+        global seventh_box
+        global eigth_box
+        global ninth_box
+        if (turno=="1"):
+            first_box=Fore.BLUE+"O"
+        elif (turno=="2"):
+            second_box=Fore.BLUE+"O"
+        elif (turno=="3"):
+            third_box=Fore.BLUE+"O"
+        elif (turno=="4"):
+            forth_box=Fore.BLUE+"O"
+        elif (turno=="5"):
+            fifth_box=Fore.BLUE+"O"
+        elif (turno=="6"):
+            sixth_box=Fore.BLUE+"O"
+        elif (turno=="7"):
+            seventh_box=Fore.BLUE+"O"
+        elif (turno=="8"):
+            eigth_box=Fore.BLUE+"O"
+        elif (turno=="9"):
+            ninth_box=Fore.BLUE+"O"
         else:
-            print("You win! ")
-            break
+            if (do_you_want_sound=="y" or do_you_want_sound=="Y" or do_you_want_sound=="yes" or do_you_want_sound=="Yes" or do_you_want_sound=="YES"):
+                speak_turns.speak_for_invalid_option()
+            print(Fore.BLUE+"Enter A Valid Option.")
+            playero_turn()
+        subprocess.run('cls' , shell=True)
 
-    if isBoardFull(board):
-        print("\nGame tie")
+    def do_you_want_to_restart_game_function():
+        subprocess.run('cls' , shell=True)
+        if (do_you_want_sound=="y" or do_you_want_sound=="Y" or do_you_want_sound=="yes" or do_you_want_sound=="Yes" or do_you_want_sound=="YES"):
+            voice.say("Do You Want To Restart Game, Type Y/yes or N/no")
+            voice.runAndWait()
+            voice.stop()
+        do_you_want_to_restart_game=input(Back.WHITE+Fore.RED+"Do You Want To Restart Game [Y \ N] : ")
+        if (do_you_want_to_restart_game=="y" or do_you_want_to_restart_game=="Y" or do_you_want_to_restart_game=="yes" or do_you_want_to_restart_game=="Yes" or do_you_want_to_restart_game=="YES"):
+            if (do_you_want_sound=="y" or do_you_want_sound=="Y" or do_you_want_sound=="yes" or do_you_want_sound=="Yes" or do_you_want_sound=="YES"):
+                voice.say("Restarting Game In 3 Seconds.")
+                voice.runAndWait()
+                voice.stop()
+            print(Back.WHITE+Fore.RED+Style.BRIGHT+"Restarting Game In 3 Seconds.")
+            time.sleep(3)
+            main_game_program()
+        elif(do_you_want_to_restart_game=="n" or do_you_want_to_restart_game=="N" or do_you_want_to_restart_game=="no" or do_you_want_to_restart_game=="No" or do_you_want_to_restart_game=="NO"):
+            subprocess.run('cls' , shell=True)
+            if (do_you_want_sound=="y" or do_you_want_sound=="Y" or do_you_want_sound=="yes" or do_you_want_sound=="Yes" or do_you_want_sound=="YES"):
+                voice.say("Thank You !, Hope You Enjoyed!")
+                voice.runAndWait()
+                voice.stop()
+            print(Back.BLACK+Fore.GREEN+"THANK YOU !!! \nHOPE YOU ENJOYED !")
+            variable_to_stop_immediate_termination_of_shell=input()
+            exit()
+        else:
+            if (do_you_want_sound=="y" or do_you_want_sound=="Y" or do_you_want_sound=="yes" or do_you_want_sound=="Yes" or do_you_want_sound=="YES"):
+                voice.say("Enter A Valid Option.")
+                voice.runAndWait()
+                voice.stop()
+            print(Back.WHITE+Fore.RED+"Enter A Valid Option.")
+            do_you_want_to_restart_game_function()
+    
+    def playerx_wins_greetings_function():
+        if (do_you_want_sound=="y" or do_you_want_sound=="Y" or do_you_want_sound=="yes" or do_you_want_sound=="Yes" or do_you_want_sound=="YES"):
+            voice.say(f"{playerx} Wins, Congratulations")
+            voice.runAndWait()
+            voice.stop()
+        print(Back.YELLOW+Fore.BLUE+f"{playerx} Wins\nCongratulations !!!")
+        time.sleep(0.5)
+        do_you_want_to_restart_game_function()
+
+    def playero_wins_greetings_function():
+        if (do_you_want_sound=="y" or do_you_want_sound=="Y" or do_you_want_sound=="yes" or do_you_want_sound=="Yes" or do_you_want_sound=="YES"):
+            voice.say(f"{playero} Wins, Congratulations")
+            voice.runAndWait()
+            voice.stop()
+        print(Back.YELLOW+Fore.RED+f"{playero} Wins\nCongratulations !!!")
+        time.sleep(0.5)
+        do_you_want_to_restart_game_function()
+    
+    i=0   
+    while(True):
+
+        playerx_turn()
+
+        if (first_box==second_box==third_box==Fore.RED+"X") :
+            playerx_wins_greetings_function()
+        elif (forth_box==fifth_box==sixth_box==Fore.RED+"X") :
+            playerx_wins_greetings_function()
+        elif (seventh_box==eigth_box==ninth_box==Fore.RED+"X") :
+            playerx_wins_greetings_function()
+        elif (first_box==forth_box==seventh_box==Fore.RED+"X") :
+            playerx_wins_greetings_function()
+        elif (second_box==fifth_box==eigth_box==Fore.RED+"X") :
+            playerx_wins_greetings_function()
+        elif (third_box==sixth_box==ninth_box==Fore.RED+"X") :
+            playerx_wins_greetings_function()
+        elif (first_box==fifth_box==ninth_box==Fore.RED+"X") :
+            playerx_wins_greetings_function()
+        elif (third_box==fifth_box==seventh_box==Fore.RED+"X") :
+            playerx_wins_greetings_function()
+
+        playero_turn()
+
+        if (first_box==second_box==third_box==Fore.BLUE+"O") :
+            playero_wins_greetings_function()
+        elif (forth_box==fifth_box==sixth_box==Fore.BLUE+"O") :
+            playero_wins_greetings_function()
+        elif (seventh_box==eigth_box==ninth_box==Fore.BLUE+"O") :
+            playero_wins_greetings_function()
+        elif (first_box==forth_box==seventh_box==Fore.BLUE+"O") :
+            playero_wins_greetings_function()
+        elif (second_box==fifth_box==eigth_box==Fore.BLUE+"O") :
+            playero_wins_greetings_function()
+        elif (third_box==sixth_box==ninth_box==Fore.BLUE+"O") :
+            playero_wins_greetings_function()
+        elif (first_box==fifth_box==ninth_box==Fore.BLUE+"O") :
+            playero_wins_greetings_function()
+        elif (third_box==fifth_box==seventh_box==Fore.BLUE+"O") :
+            playero_wins_greetings_function()
+            
+        i=i+1
+        if i==10:
+            if (do_you_want_sound=="y" or do_you_want_sound=="Y" or do_you_want_sound=="yes" or do_you_want_sound=="Yes" or do_you_want_sound=="YES"):
+                voice.say("Tie ! No One Wins.")
+                voice.runAndWait()
+                voice.stop()
+            print(Back.YELLOW+Fore.GREEN+"Tie !!!\nNo One Wins.")
+            do_you_want_to_restart_game_function()
 
 
-while True:
-    choice = input("Do you want to play a game (Y/N): ")
-    if choice.lower() == 'y':
-        board = [" " for i in range(10)]
-        print("-----------------------------------------")
-        main()
-    else:
-        break
+main_game_program()
