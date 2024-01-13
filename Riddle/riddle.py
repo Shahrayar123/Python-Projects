d203b44fcb3120e5f753d0706d0cd461bb634778
Abi0560 <abhiraman0560@gmail.com>
2024-01-14 03:48:16 +0530

    improved-python-proj-rid

--- Riddle/riddle.py
+++ Riddle/riddle.py
@@ -1,4 +1,32 @@
 import random
+import collections
+
+glob_list = collections.defaultdict(list) # a list to collect all correct values of anagram entered by user in backpack
+
+
+
+def anagram(word01,solnword,qnword,n):       
+  s=0
+  temp1="".join(sorted(word01))
+  temp2="".join(sorted(solnword))
+  if len(word01) !=n :
+    print("number of letters do not match")
+  elif word01==qnword:
+      print("U typed the same thing")            
+  else:
+    if temp1==temp2:  
+      if word01==solnword:
+          print("you are correct\ncurrent backpack is:") 
+          s+=1        
+          global glob_list
+          glob_list[temp1].append(word01)
+          glob_list[temp1].append(qnword) 
+          print(*glob_list.values())
+          
+      else:
+          print("u just typed random")
+  return s 
+
 
 # Riddle game
 
@@ -10,11 +38,13 @@ riddles = {
     "What is so fragile that saying its name breaks it?": "Silence"
 }
 
+score = 0
+
 riddle_keys = list(riddles.keys())  # Convert the dictionary keys into a list.
 
 random.shuffle(riddle_keys)  # Shuffle the list of riddle keys.
 
-score = 0
+
 
 for riddle_key in riddle_keys:
     print(riddle_key)
@@ -24,5 +54,20 @@ for riddle_key in riddle_keys:
         score += 1
     else:
         print("Wrong answer. Correct answer: " + riddles[riddle_key])
+print("\n"+str(score))        
+
+
+print("\n\nLEVEL 2\n\n")
+
+print("""An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+         typically using all the original letters exactly once. For example elbow and below""")
+
+answord1=["below","reactive","listen","flea","enraged","admirer"]
+solutionword=["elbow","creative","silent","leaf","angered","married"]
+
+for i in range(len(solutionword)):
+   word1=input("Rearrange the letters of the word  -" +answord1[i] +"-   and form another meaningful word\n")
+   score+=anagram(word1.lower(),solutionword[i],answord1[i],len(solutionword[i]))
+   print("next\n")        
 
 print("Game over! Your score is: " + str(score))
