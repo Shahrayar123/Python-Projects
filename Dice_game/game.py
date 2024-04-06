8b2c97e7fa11199ed98aefbb021cca99219fe521
manogames <manogames1222@gmail.com>
2024-04-06 13:32:50 -0600

    fixed bugs

--- Dice_game/game.py
+++ Dice_game/game.py
@@ -4,11 +4,10 @@ import time
 import random
 #Simple function to clean all terminal and detects your OS
 def clear_console():
-    match os.name:
-        case "nt":
-            os.system('clear')
-        case _ :
-            os.system('clear')
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
 #SECTION WHEN "TURNING DICE" AND GIVES A RESULT
 def turning_and_result(predict:int):
     clear_console()
@@ -33,6 +32,11 @@ def Game_Step1():
     print(ASCII_ASSETS["ready?"])
     try:
         input_number = int(input('Choise a number from 1 to 6\n\n> '))
+        if input_number > 6 or input_number < 0:
+            clear_console()
+            print("  ⚠  That's not a number... redirecting to menu")
+            time.sleep(3)
+            menu()
         turning_and_result(input_number)
     except ValueError:
         clear_console()
@@ -46,13 +50,17 @@ def menu():
     print(ASCII_ASSETS["TextMenu"])
     print(ASCII_ASSETS["Menu_Image"])
     input_menu = input('\nPress Enter to play or type "exit" to leave the game\n\n> ')
-    match input_menu:
-        case "exit":
-            exit()
-        case '' :
-            Game_Step1()
-
+    if input_menu == "":
+        Game_Step1()
+    elif input_menu == "exit":
+        exit()
     
 
 if __name__ == '__main__':
-    menu()
+    try:
+        menu()
+    except KeyboardInterrupt:
+        clear_console()
+        print("  ⚠  Leaving the game...")
+        time.sleep(1)
+        exit()
