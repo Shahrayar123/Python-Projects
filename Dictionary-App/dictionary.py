55ccc0f15f8af5454cdce60c48a624bfbc63e651
DavidZA <Davidzdbr@gmail.com>
2024-07-19 09:36:38 +0300

    Improve dictionary app functionality and readability
    - Implement formatted output for word meanings
    - Add suggestion feature for misspelled words
    - Combine check_meaning, get_antonym, and get_synonym into a single function
    - Enhance error handling for word not found cases

--- Dictionary-App/dictionary.py
+++ Dictionary-App/dictionary.py
@@ -1,21 +1,43 @@
 from PyDictionary import PyDictionary
+from difflib import get_close_matches
 
 dictionary = PyDictionary()
 
+def check_word(word, action):
+    if action == "meaning":
+        result = dictionary.meaning(word)
+        if result:
+            formatted_result = ""
+            for part_of_speech, definitions in result.items():
+                formatted_result += f"\n{part_of_speech}:\n"
+                for i, definition in enumerate(definitions, 1):
+                    formatted_result += f"  {i}. {definition}\n"
+            return formatted_result.strip()
+    elif action == "antonym":
+        result = dictionary.antonym(word)
+    elif action == "synonym":
+        result = dictionary.synonym(word)
+    else:
+        result = None
 
-def check_meaning(word):         # if user want to check meaning of word
-    return dictionary.meaning(word)
+    if result:
+        if isinstance(result, list):
+            return ", ".join(result)
+        return result
+    else:
+        common_words = ["hello", "world", "python", "programming", "dictionary", "language", "computer", "science"]
+        close_matches = get_close_matches(word, common_words, n=3, cutoff=0.6)
+        if close_matches:
+            return f"Word not found. Did you mean: {', '.join(close_matches)}?"
+        else:
+            return "Word not found and no close matches."
 
-def get_antonym(word):         # if user want to check antonym of word
-    return dictionary.antonym(word)
-
-def get_synonym(word):         # if user want to check synonym of word
-    return dictionary.synonym(word)
-
-def translate(word,language):     # if user want to translate word into other language
-    return dictionary.translate(word,language)
-
-
+def translate(word, language):
+    translation = dictionary.translate(word, language)
+    if translation:
+        return translation
+    else:
+        return "Translation not found."
 
 def menu():
     print('''
@@ -24,51 +46,36 @@ Enter 1 to check meaning of word
 Enter 2 to get antonyms of word
 Enter 3 to get synonyms of word
 Enter 4 to translate word to other language
-Enter 0 to the close dictionary
+Enter 0 to close the dictionary
 ''')
 
-    choice = int(input("Enter your choice: "))
-
+    choice = input("Enter your choice: ")
     return choice
 
-
-while(True):
+while True:
     word = input("\nEnter a word: ")
     user_choice = menu()
-    match user_choice:
-        case 0:
-            print("Dictionary is closed")
-            exit(0)
-        case 1:
-            meaning = check_meaning(word)
-            print(meaning)
-        case 2:
-            antonym = get_antonym(word)
-            print(antonym)
-        case 3:
-            synonym = get_synonym(word)
-            print(synonym)
-        case 4:
-            print('''Enter a language code in which you want to translate word
+    
+    if user_choice == '0':
+        print("Dictionary is closed")
+        break
+    elif user_choice == '1':
+        meaning = check_word(word, "meaning")
+        print(meaning)
+    elif user_choice == '2':
+        antonym = check_word(word, "antonym")
+        print(antonym)
+    elif user_choice == '3':
+        synonym = check_word(word, "synonym")
+        print(synonym)
+    elif user_choice == '4':
+        print('''Enter a language code in which you want to translate the word
 Like for URDU language code is ur
 For ARABIC language code is ar
 For HINDI language code is hi
 ''')
-
-            lang_choice = input("Enter your choice: ")
-            trans = translate(word, lang_choice)
-            print(trans)
-        case _:
-            print("Invalid choice!")
-
-#-------------------------------------------------------------
-
-# Next tasks:
-# 1- is to make this program in such a way that if word not exist then do something
-# 2- also if user enter wrong word by mistake (for example if user want to enter ""happy"" but type "Haappyy") then do something
-# 3- task number 2 is with get_close_matches() for difflib library
-# 4- then make GUI application of this
-
-#-------------------------------------------------------------
-
-
+        lang_choice = input("Enter your choice: ")
+        trans = translate(word, lang_choice)
+        print(trans)
+    else:
+        print("Invalid choice!")
